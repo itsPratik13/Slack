ec5b94b720f7fdd2e8f26efc912c573716405343
Pratik <survasepratik1304@gmail.com>
2025-02-22 01:03:31 +0530

    Setting up the project

---
+++ slack/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return(
+    <div className="font-bold text-4xl">Hello world
+    
+    <Button size="default">Click me</Button>
+ 
+    </div>
+
+);
+    
+}
