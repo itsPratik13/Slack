ebbbd2222b1c379b9c8559b611359ca12aa85c47
Pratik <survasepratik1304@gmail.com>
2025-03-16 16:49:58 +0530

    User profile button and log out functionality

--- slack/src/app/page.tsx
+++ slack/src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 import { useAuthActions } from "@convex-dev/auth/react";
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@/features/auth/components/user-button";
 
 const Home=()=>{
   const{signOut}=useAuthActions();
   return(
    <div>
-    <Button onClick={()=> signOut()}>Sign out</Button>
-    Logged in!
+    
+   
+    <UserButton/>
    </div>
     
     
--- slack/src/features/auth/components/sign-up-card.tsx
+++ slack/src/features/auth/components/sign-up-card.tsx
@@ -1,7 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { TriangleAlert } from "lucide-react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -10,133 +14,142 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Separator } from "@radix-ui/react-separator";
+import { Separator } from "@/components/ui/separator";
 import { SignInFlow } from "../types";
-import { TriangleAlert } from "lucide-react";
-import { useAuthActions } from "@convex-dev/auth/react";
 
 interface SignUpCardProps {
   setState: (state: SignInFlow) => void;
 }
 
 export const SignUpCard = ({ setState }: SignUpCardProps) => {
-  const { signIn } = useAuthActions();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmpassword, setConfirmPassword] = useState("");
-  const [pending, SetPending] = useState(false);
-  const[error,setError]=useState("");
+  const [signingUp, setSigningUp] = useState(false);
+  const [error, setError] = useState("");
 
-  const OnProviderSignUp = (value: "github" | "google") => {
-    SetPending(true);
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const { signIn } = useAuthActions();
+
+  const handlePasswordSignUp = form.handleSubmit(
+    ({ name, email, password, confirmPassword }) => {
+      if (password !== confirmPassword) {
+        setError("Passwords do not match");
+        return;
+      }
+      setSigningUp(true);
+      signIn("password", { name, email, password, flow: "signUp" })
+        .catch(() => {
+          setError("Something went wrong!");
+        })
+        .finally(() => {
+          setSigningUp(false);
+        });
+    }
+  );
+
+  const handleProviderSignUp = (value: "github" | "google") => () => {
+    setSigningUp(true);
     signIn(value).finally(() => {
-      SetPending(false);
+      setSigningUp(false);
     });
   };
-  const onPasswordSignUp=(e:React.FormEvent<HTMLFormElement>)=>{
 
-    e.preventDefault();
-    if(password!==confirmpassword){
-      setError("Passwords don't match");
-      return;
-    }
-    SetPending(true);
-    signIn(password,{email,password,flow:"signUp"})
-    .catch(()=>{
-      setError("Something went wrong!")
-    }).finally(()=>{
-      SetPending(false);
-    })
-
-  }
   return (
-    <Card className="h-full w-full p-8">
+    <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
-        <CardTitle className="font-bold text-2xl">
-          Sign Up to continue
-        </CardTitle>
+        <CardTitle>Sign up to continue</CardTitle>
         <CardDescription>
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
-      {!!error && (
-   <div className="bg-destructive/15 p-3 rounded-md flex items-center text-sm text-destructive gap-x-2 mb-6">
-       <TriangleAlert className="h-4 w-4"/>
-       <p>{error}</p>
-   </div>
- )}
-
+      {error && (
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+          <TriangleAlert className="size-4" />
+          <p>{error}</p>
+        </div>
+      )}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form  onSubmit={onPasswordSignUp} className="space-y-2.5">
+        <form className="space-y-2.5" onSubmit={handlePasswordSignUp}>
           <Input
-            disabled={pending}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            {...form.register("name", {
+              required: true,
+            })}
+            disabled={signingUp}
+            placeholder="Full name"
+          />
+          <Input
+            {...form.register("email", {
+              required: true,
+            })}
+            disabled={signingUp}
             placeholder="Email"
             type="email"
-            required
           />
           <Input
-            disabled={pending}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            {...form.register("password", {
+              required: true,
+            })}
+            disabled={signingUp}
             placeholder="Password"
             type="password"
-            required
           />
           <Input
-            disabled={pending}
-            value={confirmpassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            placeholder="Confirm Password"
+            {...form.register("confirmPassword", {
+              required: true,
+            })}
+            disabled={signingUp}
+            placeholder="Confirm password"
             type="password"
-            required
           />
           <Button
             type="submit"
-            className="w-full "
+            className="w-full"
             size="lg"
-            disabled={pending}
+            disabled={signingUp}
             variant="slack"
           >
             Continue
           </Button>
         </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            disabled={signingUp}
+            onClick={handleProviderSignUp("google")}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FcGoogle className="size-5 absolute top-3 left-2.5" />
+            Continue with Google
+          </Button>
+          <Button
+            disabled={signingUp}
+            onClick={handleProviderSignUp("github")}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FaGithub className="size-5 absolute top-3 left-2.5" />
+            Continue with Github
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Already have an account?{" "}
+          <span
+            className="text-sky-700 hover:underline cursor-pointer"
+            onClick={() => setState("signIn")}
+          >
+            Sign in
+          </span>
+        </p>
       </CardContent>
-      <Separator className="my-4" />
-
-      <div className="flex flex-col gap-y-2.5">
-        <Button
-          disabled={pending}
-          onClick={() => OnProviderSignUp("google")}
-          variant="outline"
-          size="lg"
-          className="w-full relative"
-        >
-          <FcGoogle className="size-5 absolute left-3 top-2.5" />
-          Continue with Google
-        </Button>
-
-        <Button
-          disabled={pending}
-          onClick={() => OnProviderSignUp("github")}
-          variant="outline"
-          size="lg"
-          className="w-full relative"
-        >
-          <FaGithub className="size-5 absolute left-3 top-2.5" />
-          Continue with Github
-        </Button>
-      </div>
-      <p className=" my-3 text-sx text-muted-foreground">
-        Already have an account?{" "}
-        <span
-          onClick={() => setState("signIn")}
-          className="text-sky-700 hover:underline cursor-pointer"
-        >
-          SignIn!
-        </span>
-      </p>
     </Card>
   );
 };
