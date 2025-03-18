a727daa4ba0cf224e7d0e4b6fd1e76be8f5a6368
Pratik <survasepratik1304@gmail.com>
2025-03-18 17:34:10 +0530

    Partial workplace creation

--- slack/convex/workspaces.ts
+++ slack/convex/workspaces.ts
@@ -1,4 +1,27 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
+
+export const create=mutation({
+    args:{
+        name:v.string(),
+    },
+    handler:async (ctx,args)=>{
+        const userId=await getAuthUserId(ctx)
+        if(!userId){
+            throw new Error("unAuthorized")
+        }
+        const joinCode="12345";
+        const workspaceId=await ctx.db.insert("workspaces",{
+         name:args.name,
+         userId,
+         joinCode
+        })
+
+        return workspaceId;
+    }
+
+})
 
 export const get=query({
     args:{},
--- slack/src/features/workspaces/components/create-workspace-modal.tsx
+++ slack/src/features/workspaces/components/create-workspace-modal.tsx
@@ -1,32 +1,51 @@
 "use client";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { useCreateWorkSpaceModal } from "../storage/use-create-workspace-modal"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useCreateWorkSpaceModal } from "../storage/use-create-workspace-modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
 
-  export const CreateWorkSpaceModal=()=>{
-    const[open,setOpen]=useCreateWorkSpaceModal();
+export const CreateWorkSpaceModal = () => {
+  const [open, setOpen] = useCreateWorkSpaceModal();
 
-    const handleClose=()=>{
-        setOpen(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const form = useForm({
+    defaultValues: {
+      name: "",
+    },
+  });
 
-    }
-
-    return(
-        <Dialog open={open} onOpenChange={handleClose}>
-            <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>
-                    Add a workspace
-                  </DialogTitle>
-                </DialogHeader>
-            </DialogContent>
-
-        </Dialog>
-    )
-  }
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a Workspace</DialogTitle>
+        </DialogHeader>
+        <form className="space-y-4">
+          <Input
+            {...form.register("name", {
+              required: true,
+              minLength: 3,
+              maxLength: 100,
+            })}
+            disabled={false}
+            autoFocus
+            placeholder="Workspace name ex:'Home', 'Work'"
+          />
+          <div className="flex justify-end">
+            <Button disabled={false} variant="slack">Create </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
